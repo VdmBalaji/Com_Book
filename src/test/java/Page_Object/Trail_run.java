5c12f74f9877d38ac3de27f2146eef3f33299022
Balaji <Balaji@LAPTOP-V4EQ9E0B>
2024-08-27 14:18:24 +0530

    updated - 2pm

--- src/test/java/Page_Object/Trail_run.java
+++ src/test/java/Page_Object/Trail_run.java
@@ -4,5 +4,12 @@ public class Trail_run {
 
 	public void test_pull() {
 	System.out.println("Trail chekc");
+	
+	
+	
+	}
+	public void met2()
+	{
+		System.out.println("System print");
 	}
 }
---
+++ src/test/java/Utilities/helper_delete_asap.java
@@ -0,0 +1,5 @@
+package Utilities;
+
+public class helper_delete_asap {
+
+}
